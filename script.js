4c7dc13e34f21c34c354601f06392d3df78b67c8
vrushali <vrushalidevlekar12@gmail.com>
2025-07-24 20:38:02 +0530

    js function bubble game complete

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+var timer = 10;
+var score = 0;
+var rn;
+function scoreValue() {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
+}
+function hitValue() {
+  rn = Math.floor(Math.random() * 9) + 1;
+  document.querySelector("#hitval").textContent = rn;
+}
+
+function timerStart() {
+  var timerint = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    } else {
+      clearTimeout(timerint);
+      document.querySelector("#pbottom").innerHTML = `<div class="over">
+                <h1>Game over and your score was ${score} 🥳</h1>
+            </div>`;
+    }
+  }, 1000);
+}
+
 function makebubble(num) {
   var clutter = "";
 
@@ -8,4 +34,16 @@ function makebubble(num) {
   document.querySelector("#pbottom").innerHTML = clutter;
 }
 
-// makebubble(122);
+document.querySelector("#pbottom").addEventListener("click", function (dets) {
+  var clickedNum = Number(dets.target.textContent);
+  if (clickedNum == rn) {
+    scoreValue();
+    makebubble(105);
+    hitValue();
+  } else {
+    // score -= 10;
+  }
+});
+hitValue();
+makebubble(105);
+timerStart();
